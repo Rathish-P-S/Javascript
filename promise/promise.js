fd786ce3fd402eadce00d243eedbe43e6ae105c7
Rathish-P-S <124355202+Rathish-P-S@users.noreply.github.com>
2023-03-02 10:47:35 +0530

    Create promise.js

---
+++ promise/promise.js
@@ -0,0 +1,208 @@
+function prepareIceCream(flavor) {
+    console.log(`Preparing ${flavor} ice cream...`);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (flavor === "chocolate") {
+          reject("We don't have chocolate ice cream today!");
+        } else {
+          console.log(`${flavor} ice cream is ready!`);
+          resolve();
+        }
+      }, 2000);
+    });
+  }
+  
+  function addToppings(toppings) {
+    console.log(`Adding ${toppings.join(", ")}...`);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(`Toppings added: ${toppings.join(", ")}`);
+        resolve();
+      }, 1000);
+    });
+  }
+  
+  function serveIceCream() {
+    console.log("Ice cream served!");
+  }
+  
+  prepareIceCream("vanilla")
+    .then(() => {
+      return addToppings(["chocolate chips", "sprinkles"]);
+    })
+    .then(() => {
+      serveIceCream();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+const promise1=new Promise((resolve,reject)=>
+{
+    resolve("Success");
+});
+promise1.then((value)=>
+{
+    console.log(value);
+});
+const promise2=new Promise((dos,dont)=>
+{
+    dont()
+    {
+        console.log("An error  is catched-first");
+    }
+})
+promise2.then(result=>
+    {
+      console.log(result);
+      return "An error is catched second time";
+    })
+    .catch(error=>
+        {
+            console.log(error);
+            return "Second op completed";
+        })
+const promise_1=Promise.resolve(3);
+const promise_2=42;
+const promise_3=new Promise((resolve,reject)=>
+{
+    setTimeout(resolve,100,'foo');
+});
+const promise_4=new Promise((reject,resolve)=>
+{
+    setTimeout(reject,100,'zoo');
+});
+Promise.all([promise_1,promise_2,promise_3,promise_4]).then(values=>{
+    console.log(values);
+});
+const promises=[promise_1,promise_2,promise_3,promise_4];
+Promise.allSettled(promises).then(val=>
+    val.forEach(vale=>console.log(vale.status)));
+console.log(Promise.promise_4);
+
+//Promise any()
+const anyPromise1=new Promise((resolve,reject)=>
+{
+    setTimeout(resolve,500,'quitslow');
+})
+const anyPromise2=new Promise((reject,resolve)=>
+{
+    setTimeout(reject,100,'quick');
+});
+
+const anyPromises=[anyPromise1,anyPromise2];
+Promise.any(anyPromises).then((values)=> console.log(values));//This returned promise fulfills when any of the input's promises fulfills, with this first fulfillment value. 
+//Promise.any()-Output is based on Promise.race().
+
+//Promise.catch()-It schedules a function when promise is rejected.
+
+const catchPromise1=new Promise((resolve,reject)=>
+    {
+        throw new Error('osssh-no');
+    })
+catchPromise1.catch((error)=>{console.error(error)});
+//Finally()
+
+function checkMail()
+{
+    let vari=1;
+    return new Promise((resolve,reject)=>
+    {
+        if(vari)
+        {
+            resolve('Mail has arrived');
+
+        }
+        else{
+            reject(new Error("Failed to arrive"));
+        }
+    });
+}
+checkMail()
+
+    .then((mail)=>
+    {
+        console.log(mail)
+    })
+    // .catch ( (err)=>{console.error(err);})
+    .catch((err) => {
+        console.error(err);
+      })
+      .finally(()=>{
+          console.log("ALl experiment done");
+      })
+
+      //Promise.reject()
+
+      function resolved(reslt)
+      {
+        console.log(reslt);
+      }
+      function rejected(reslt)
+      {
+        console.log(reslt);
+      }
+   
+   // Promise.resolve(new Error('kavin')).then(resolved,rejected);
+
+   const p= Promise.resolve(3);
+   p.then((value)=>{console.log(value);})]
+   
+    //chaining then()
+
+   const a=new Promise((resolve,reject)=>
+   {
+    setTimeout(()=>{
+    resolve(10);},1000);
+   });
+   a.then(result => {console.log(result);
+   return result+2;})
+   .then(result =>{console.log(result);})
+   
+    //await()
+   function getData()
+   {
+    return new Promise((resolve,reject)=>
+    {
+        setTimeout(()=>
+        {
+            resolve('Waited 5 seconds');
+        },5000);
+    })
+   }
+
+   async function fetchdata()
+   {  
+    //console.log('calling await');
+    const result=await getData();
+    console.log(result);
+    console.log("HI")
+   }
+   fetchdata();
+
+//chaining await
+
+   function getData()
+   {
+    return new Promise((resolve,reject)=>
+    {
+        setTimeout(()=>
+        {
+            resolve('Waited 5 seconds');
+        },5000);
+    })
+   }
+   async function ProcessData()
+   {
+    const result= await getData();
+    return result.toUpperCase();
+   }
+
+   async function fetchdata()
+   {  
+    //console.log('calling await');
+    const result=await ProcessData();
+    console.log(result);
+    console.log("HI")
+   }
+   fetchdata();
